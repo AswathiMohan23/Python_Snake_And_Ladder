82ec29b2434a22ca76df5d4fc79bdc97043279d9
AswathiMohan23 <achu999here@gmail.com>
2023-01-26 10:04:01 +0530

    [ASWATHI] added . code to move ahead for ladder and behind for snake

---
+++ Snake_And_Ladder/game_main.py
@@ -0,0 +1,4 @@
+from Snake_And_Ladder.game_methods import game_methods
+
+methods = game_methods()
+methods.game()
--- Snake_And_Ladder/game_methods.py
+++ Snake_And_Ladder/game_methods.py
@@ -1,12 +1,39 @@
-from random import randint
+from Snake_And_Ladder.dice_check import dice_check
 
 
-def randomCheck():
-    random_number = randint(1, 6)
-    print("Number obtained by player = ", random_number)
-    return random_number
+class game_methods(dice_check):
+    def __init__(self):
+        self.win = 0
+        self.value = 0
 
+    def ladder(self, ladder_value):
+        print("\n WOW IT'S A LADDER ------->>>>>>",end="")
+        print("\t\tprevious position : ", self.value)
+        print("\t\t\t\t\t\t\t\t\t\tyou got : ", ladder_value)
+        self.value = self.value + ladder_value
+        print("\t\t\t\t\t\t\t\t\t\tmove ahead by ", ladder_value)
+        print("\t\t\t\t\t\t\t\t\t\tyour current position : ", self.value)
 
-def optionCheck():
-    option = randint(0, 2)
-    return option
+    def snake(self, snake_value):
+        print("\nOOPS IT'S A SNAKE ------>>>>>> ",end="")
+        print("\t\tprevious position : ", self.value)
+        print("\t\t\t\t\t\t\t\t\tyou got : ", snake_value)
+        self.value = self.value - snake_value
+        if self.value > 0:
+            print("\t\t\t\t\t\t\t\t\tmove behind by ", snake_value)
+            print("\t\t\t\t\t\t\t\t\tyour current position : ", self.value)
+        elif self.value <= 0:
+            print("\t\t\t\t\t\t\t\t\toops!!! go back to zero")
+
+    def game(self):
+        check = self.optionCheck()
+        if check == 1:
+            ladder_value = self.randomCheck()
+            self.ladder(ladder_value)
+        elif check == 2:
+            snake_value = self.randomCheck()
+            self.snake(snake_value)
+
+        else:
+            print("\nYou dont have chance to play so stay at the same position till your next turn")
+
