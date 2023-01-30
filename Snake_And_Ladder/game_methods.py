af6785345131ac603a171dbaf2d73b4b0f5e4f17
AswathiMohan23 <achu999here@gmail.com>
2023-01-30 08:54:53 +0530

    [ASWATHI] added. code to play the game till winning psition of 100 is reached

--- Snake_And_Ladder/game_methods.py
+++ Snake_And_Ladder/game_methods.py
@@ -7,15 +7,18 @@ class game_methods(dice_check):
         self.value = 0
 
     def ladder(self, ladder_value):
-        print("\n WOW IT'S A LADDER ------->>>>>>",end="")
+        print("\n WOW IT'S A LADDER ------->>>>>>", end="")
         print("\t\tprevious position : ", self.value)
         print("\t\t\t\t\t\t\t\t\t\tyou got : ", ladder_value)
         self.value = self.value + ladder_value
         print("\t\t\t\t\t\t\t\t\t\tmove ahead by ", ladder_value)
-        print("\t\t\t\t\t\t\t\t\t\tyour current position : ", self.value)
+        if self.value >= 100:
+            print("\t\t\t\t\t\t\t\t\t\tyour current position : You won")
+        else:
+            print("\t\t\t\t\t\t\t\t\t\tyour current position : ", self.value)
 
     def snake(self, snake_value):
-        print("\nOOPS IT'S A SNAKE ------>>>>>> ",end="")
+        print("\nOOPS IT'S A SNAKE ------>>>>>> ", end="")
         print("\t\tprevious position : ", self.value)
         print("\t\t\t\t\t\t\t\t\tyou got : ", snake_value)
         self.value = self.value - snake_value
@@ -24,16 +27,20 @@ class game_methods(dice_check):
             print("\t\t\t\t\t\t\t\t\tyour current position : ", self.value)
         elif self.value <= 0:
             print("\t\t\t\t\t\t\t\t\toops!!! go back to zero")
+            self.value = 0
 
     def game(self):
-        check = self.optionCheck()
-        if check == 1:
-            ladder_value = self.randomCheck()
-            self.ladder(ladder_value)
-        elif check == 2:
-            snake_value = self.randomCheck()
-            self.snake(snake_value)
+        while self.value >= 100:
+            print("You won the game")
+        while self.value < 100:
+            check = self.optionCheck()
+            if check == 1:
+                ladder_value = self.randomCheck()
+                self.ladder(ladder_value)
+            elif check == 2:
+                snake_value = self.randomCheck()
+                self.snake(snake_value)
 
-        else:
-            print("\nYou dont have chance to play so stay at the same position till your next turn")
+            else:
+                print("\nYou dont have chance to play so stay at the same position till your next turn")
 
