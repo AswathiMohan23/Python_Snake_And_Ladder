e88096ca5de09985d303583eb15969a6ba506044
AswathiMohan23 <achu999here@gmail.com>
2023-01-30 17:20:47 +0530

    [ASWATHI] added. code to play the game till winning psition of 100 is reached

--- Snake_And_Ladder/game_main.py
+++
@@ -1,4 +0,0 @@
-from Snake_And_Ladder.game_methods import game_methods
-
-methods = game_methods()
-methods.game()
--- Snake_And_Ladder/game_methods.py
+++ Snake_And_Ladder/game_methods.py
@@ -1,46 +1,70 @@
-from Snake_And_Ladder.dice_check import dice_check
+from random import randint
 
 
-class game_methods(dice_check):
+
+
+class game_methods:
+    current_position = 0
+    previous_position = 0
     def __init__(self):
-        self.win = 0
-        self.value = 0
+        self.ladder_value = None
+        self.snake_value = None
 
-    def ladder(self, ladder_value):
+    def randomCheck(self):
+        random_number = randint(1, 6)
+        return random_number
+
+    def optionCheck(self):
+        option = randint(0, 2)
+        return option
+
+    def ladder(self,ladder_value):
         print("\n WOW IT'S A LADDER ------->>>>>>", end="")
-        print("\t\tprevious position : ", self.value)
+        print("\t\tprevious position : ", self.previous_position)
         print("\t\t\t\t\t\t\t\t\t\tyou got : ", ladder_value)
-        self.value = self.value + ladder_value
+        self.current_position = self.previous_position + ladder_value
         print("\t\t\t\t\t\t\t\t\t\tmove ahead by ", ladder_value)
-        if self.value >= 100:
-            print("\t\t\t\t\t\t\t\t\t\tyour current position : You won")
+        if self.current_position == 100:
+            print("\t\t\t\t\t\t\t\t\t\tyour current position : 100 !!!! \n\t\t\t\t\t\t\t\t\t\tYou won")
+        elif self.current_position > 100:
+            print("\t\t\t\t\t\t\t\t\t\tyour current position : ", self.current_position, "\n\t\t\t\t\t\t\t\t\t\tyou reached beyond 100 so go back to your previous position and wait till you get 100")
+            self.current_position = self.previous_position
         else:
-            print("\t\t\t\t\t\t\t\t\t\tyour current position : ", self.value)
+            print("\t\t\t\t\t\t\t\t\t\tyour current position : ",self.current_position)
+            self.previous_position = self.current_position
 
-    def snake(self, snake_value):
+    def snake(self,snake_value):
         print("\nOOPS IT'S A SNAKE ------>>>>>> ", end="")
-        print("\t\tprevious position : ", self.value)
+        print("\t\tprevious position : ", self.previous_position)
         print("\t\t\t\t\t\t\t\t\tyou got : ", snake_value)
-        self.value = self.value - snake_value
-        if self.value > 0:
+        self.current_position = self.previous_position - snake_value
+        if self.current_position > 0:
             print("\t\t\t\t\t\t\t\t\tmove behind by ", snake_value)
-            print("\t\t\t\t\t\t\t\t\tyour current position : ", self.value)
-        elif self.value <= 0:
+            print("\t\t\t\t\t\t\t\t\tyour current position : ", self.current_position)
+            self.previous_position = self.current_position
+        elif self.current_position <= 0:
+            self.previous_position = 0
             print("\t\t\t\t\t\t\t\t\toops!!! go back to zero")
-            self.value = 0
+            print("\t\t\t\t\t\t\t\t\tyour current position : ", 0)
 
     def game(self):
-        while self.value >= 100:
+        if self.current_position == 100:
             print("You won the game")
-        while self.value < 100:
+
+        while self.current_position < 100:
             check = self.optionCheck()
             if check == 1:
-                ladder_value = self.randomCheck()
-                self.ladder(ladder_value)
+                self.ladder_value = self.randomCheck()
+                self.ladder(self.ladder_value)
             elif check == 2:
-                snake_value = self.randomCheck()
-                self.snake(snake_value)
+                self.snake_value = self.randomCheck()
+                self.snake(self.snake_value)
 
             else:
                 print("\nYou dont have chance to play so stay at the same position till your next turn")
 
+
+if __name__ =="__main__":
+
+    methods = game_methods()
+    methods.game()
